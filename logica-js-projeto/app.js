a056e6836fd5d969e7d1f9f89415aa1ed2b49e5f
marcelasam <marcelasampp@gmail.com>
2025-10-15 15:32:19 -0300

    tentativA

--- logica-js-projeto/app.js
+++ logica-js-projeto/app.js
@@ -7,22 +7,34 @@ let tentativas = 1;
 // código omitido
 
 // enquanto chute não for igual ao n.s.
-    while (chute != numeroSecreto) {
-        chute = prompt('Escolha um número entre 1 e 100');
-    
-        // se chute for igual ao número secreto
-        if (chute == numeroSecreto) {
-            alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+while (chute != numeroSecreto) {
+    chute = prompt('Escolha um número entre 1 e 100');
+    // se chute for igual ao número secreto
+    if (chute == numeroSecreto) {
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O número secreto é menor que ${chute}`);
         } else {
-            if (chute > numeroSecreto) {
-                alert(`O número secreto é menor que ${chute}`);
-            } else {
-                alert(`O número secreto é maior que ${chute}`);
-            }
-            // tentativas = tentativas + 1;
-            tentativas++;
+            alert(`O número secreto é maior que ${chute}`);
         }
-    
+        // tentativas = tentativas + 1;
+        tentativas++;
     }
 
+}
+
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+
+// desafio 03
+// se tentativas for maior que 1, palavraTentativa = 'tentativas', senão, palavraTentativa = 'tentativa'
+
+
+
+// if (tentativas > 1) {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativas!`);
+// } else {
+//     alert(`Isso ai! Você descobriu o número secreto ${numeroSecreto} com ${tentativas} tentativa!`);
+// }
 
